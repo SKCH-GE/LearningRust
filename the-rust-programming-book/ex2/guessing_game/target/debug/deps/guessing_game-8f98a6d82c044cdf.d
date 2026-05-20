d929b99a98d527a28d8399479fea645e276ff9f3
chiheb <saklychiheb45@gmail.com>
2026-05-20 23:34:15 +0100

    enlightment: you can open the rust project (here -tested on "guessing_game") as workspace, and still manage Git repo from the parent folder

---
+++ the-rust-programming-book/ex2/guessing_game/target/debug/deps/guessing_game-8f98a6d82c044cdf.d
@@ -0,0 +1,5 @@
+C:\dev\learningRust\the-rust-programming-book\ex2\guessing_game\target\debug\deps\guessing_game-8f98a6d82c044cdf.d: src\main.rs
+
+C:\dev\learningRust\the-rust-programming-book\ex2\guessing_game\target\debug\deps\libguessing_game-8f98a6d82c044cdf.rmeta: src\main.rs
+
+src\main.rs:
---
+++ the-rust-programming-book/ex2/guessing_game/target/debug/deps/guessing_game-fcd0059dc58de59b.d
@@ -0,0 +1,5 @@
+C:\dev\learningRust\the-rust-programming-book\ex2\guessing_game\target\debug\deps\guessing_game-fcd0059dc58de59b.d: src\main.rs
+
+C:\dev\learningRust\the-rust-programming-book\ex2\guessing_game\target\debug\deps\libguessing_game-fcd0059dc58de59b.rmeta: src\main.rs
+
+src\main.rs:
