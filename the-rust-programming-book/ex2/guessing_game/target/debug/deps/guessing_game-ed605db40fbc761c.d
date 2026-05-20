a5574da059b6969604605678553b88e2b442e29f
chiheb <saklychiheb45@gmail.com>
2026-05-20 01:05:34 +0100

    finished chapter 1

---
+++ the-rust-programming-book/ex2/guessing_game/target/debug/deps/guessing_game-ed605db40fbc761c.d
@@ -0,0 +1,5 @@
+C:\dev\learningRust\the-rust-programming-book\ex2\guessing_game\target\debug\deps\guessing_game-ed605db40fbc761c.d: src\main.rs
+
+C:\dev\learningRust\the-rust-programming-book\ex2\guessing_game\target\debug\deps\guessing_game-ed605db40fbc761c.exe: src\main.rs
+
+src\main.rs:
---
+++ the-rust-programming-book/ex2/guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\dev\learningRust\the-rust-programming-book\ex2\guessing_game\target\debug\guessing_game.exe: C:\dev\learningRust\the-rust-programming-book\ex2\guessing_game\src\main.rs
