12b210d1e4bd29e5e6c383c20d950de68e0d98b7
chiheb <saklychiheb45@gmail.com>
2026-05-21 01:03:51 +0100

    nested actions with the () { .. => ..} is still vague for me, however we can set a full block of instruction in one option ";", plus we can set as many handlers with ","

--- the-rust-programming-book/ex2/guessing_game/src/main.rs
+++ the-rust-programming-book/ex2/guessing_game/src/main.rs
@@ -4,26 +4,33 @@ use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
-    
-    // let creates a variable, by default immutable
-    let mut guess = String::new();
     let secret_number= rand::thread_rng().gen_range(1,101);
+    loop {
+        println!("Please input your guess.");
+        
+        // let creates a variable, by default immutable
+        let mut guess = String::new();
+        
+        io::stdin() .read_line(&mut guess)
+                    // Chapter 4 will better explain how references "&" work
+                    .expect("Failed to read line");
+                    // this applies to read_line like "io::stdin().read_line(&mut guess).expect("Failed to read line");"
     
-    io::stdin() .read_line(&mut guess)
-                // Chapter 4 will better explain how references "&" work
-                .expect("Failed to read line");
-                // this applies to read_line like "io::stdin().read_line(&mut guess).expect("Failed to read line");"
-
-    let guess:u32 = guess.trim().parse()
-                        .expect("Please type a number");
+        let guess:u32 = guess.trim().parse()
+                            .expect("Please type a number");
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small!"),
+        }
     
-    match guess.cmp(&secret_number) {
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Less => println!("Too small!"),
+        println!("You guessed {}", guess);
+        println!("the secret number: {}", secret_number);
     }
 
-    println!("You guessed {}", guess);
-    println!("the secret number: {}", secret_number);
+        
 }
